6e78c79688d5916901f714bbeefc2d4445d46f32
George Sebastian <georgesebastian4321@gmail.com>
2024-07-13 09:27:11 +0530

    empApp

---
+++ app/src/main/java/com/example/employeeapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.employeeapp;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.reg);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"registered",Toast.LENGTH_SHORT).show();
+            }
+            });
+    }
+}
